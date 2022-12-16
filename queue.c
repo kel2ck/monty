6b3994e7e52991361fac42bfdec01f836a8df60a
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-12-16 02:46:43 -0800

    update

--- queue.c
+++ queue.c
@@ -15,7 +15,6 @@ void f_queue(stack_t **head, unsigned int counter)
 /**
  * addqueue - add node to the tail stack
  * @n: new_value
- * @head: head of the stack
 */
 void addqueue(stack_t **head, int n)
 {
